7505753b8293d1d687d44e99ac1b5667e6c9ee04
devadaviddd <s3927112@rmit.edu.vn>
2026-01-10 16:04:49 +0700

    fix cookie cross site using lax

--- apps/api-gateway/src/apis/auth/controllers/admin-auth.controller.ts
+++ apps/api-gateway/src/apis/auth/controllers/admin-auth.controller.ts
@@ -130,14 +130,14 @@ export class AdminAuthController {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 30 * 60 * 1000, // 30 minutes
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   }
--- apps/api-gateway/src/apis/auth/controllers/applicant-auth.controller.ts
+++ apps/api-gateway/src/apis/auth/controllers/applicant-auth.controller.ts
@@ -134,14 +134,14 @@ export class ApplicantAuthController {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 30 * 60 * 1000, // 30 minutes
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   }
